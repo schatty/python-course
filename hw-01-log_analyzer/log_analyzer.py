c0a689bdd7e0b2bc375734d18838e31d574f5628
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2020-01-02 09:58:55 +0300

    Basic log parsing

--- hw-01-log_analyzer/log_analyzer.py
+++ hw-01-log_analyzer/log_analyzer.py
@@ -1,11 +1,6 @@
 import gzip
 from collections import defaultdict
 
-# log_format ui_short $remote_addr  $remote_user $http_x_real_ip [$time_local] 
-#                   "$request" ''$status $body_bytes_sent "$http_referer"
-#                   "$http_user_agent" "$http_x_forwarded_for" 
-#                   "$http_X_REQUEST_ID" "$http_X_RB_USER" 
-#                   $request_time;
 
 config = {
     "REPORT_SIZE": 1000,
@@ -13,29 +8,75 @@ config = {
     "LOG_DIR": "./log"
 }
 
+def split_by_space(s, item_symbols=("\"", "[", "]")):
+    """
+    Split line by spaces but do not split contenst inside quotes.
+    """
+    items = []
+    i_start = 0
+    inside_item = False
+    for i in range(len(s)):
+        if s[i] in item_symbols:
+            inside_item = not inside_item
+        if s[i] == " ":
+            if not inside_item:
+                if i - i_start > 0:
+                    items.append(s[i_start:i])
+                i_start = i+1
+    # Last item
+    if len(s) - 1 - i_start > 0:
+        items.append(s[i_start:i])
+    return items
+
+
+def parse_line(s):
+    """
+    Parse log line according to log format.
+
+
+    log_format ui_short $remote_addr  $remote_user $http_x_real_ip [$time_local] 
+                  "$request" ''$status $body_bytes_sent "$http_referer"
+                  "$http_user_agent" "$http_x_forwarded_for" 
+                  "$http_X_REQUEST_ID" "$http_X_RB_USER" 
+                  $request_time;
+    """
+    item_names = ["remote_addr", "remote_user", "http_x_real_ip", "time_local", 
+            "request", "status", "body_bytes_sent", "http_referer", 
+            "http_user_agent", "http_x_forwarded_for",
+            "http_X_REQUEST_ID", "http_X_RB_USER", "request_time"]
+    item_vals = split_by_space(s)
+    return {k:v for k, v in zip(item_names, item_vals)}
+
+
+def process_log_line(log_dict):
+    """
+    Process elements of log line.
+    """
+    def remove_redundant(s):
+        to_remove = ["\\n", "\\"]
+        for c in to_remove:
+            s = s.replace(c, "")
+        return s
+
+    log_dict["request_time"] = float(remove_redundant(log_dict["request_time"]))
+    return log_dict
+
+
+def open_logfile(path):
+    if path.endswith(".gz"):
+        return gzip.open(path, 'rb')
+    return open(path, 'r') 
+
 
 def main():
+    path = 'nginx-access-ui.log-20170629'
+    data = []
+    with open_logfile(path) as f:
+        for i, line in enumerate(f.readlines()):
+            data.append(process_log_line(parse_line(str(line))))
 
-    path = 'nginx-access-ui.log-20170630.gz'
-    data = defaultdict(dict)
-    with gzip.open(path, 'rb') as f:
-        for line in f.readlines():
-            print(line)
-            remote_addr, \
-                remote_user, \
-                http_x_real_ip, \
-                time_local, \
-                request, \
-                status, \
-                body_bytes_sent, \
-                http_referer, \
-                http_user_agent, \
-                http_x_forwarded_for, \
-                http_x_request_id, \
-                http_x_rb_user, \
-                request_time = str(line).split(' ')
-            print("Request: ", request)
-            break
+    print(f"Log contains {len(data)} records") 
+
 
 
 
